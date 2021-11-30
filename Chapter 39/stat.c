7e9699a1a0c8c0e846b9c442e89259bb5e629330
johnwesely <johnpwesely@gmail.com>
2021-11-29 17:47:15 -0700

    Add files via upload

--- Chapter 39/stat.c
+++ Chapter 39/stat.c
@@ -32,10 +32,30 @@ int main(int argc, char* argv[]) {
     }
     name_buff[buff_index] = 0;
 
+    // convert file type and mode string 
+    int m = s.st_mode;
+    char mode_buff[11] = { 0 };
+    size_t m_i = 0;
+    if (S_ISDIR(m)) {
+        sprintf(mode_buff, "d---------");
+    } else {
+        mode_buff[m_i++] = '-';
+        mode_buff[m_i++] = (m & S_IRUSR) ? 'r' : '-';
+        mode_buff[m_i++] = (m & S_IWUSR) ? 'w' : '-';
+        mode_buff[m_i++] = (m & S_IXUSR) ? 'x' : '-';
+        mode_buff[m_i++] = (m & S_IRGRP) ? 'r' : '-';
+        mode_buff[m_i++] = (m & S_IWGRP) ? 'w' : '-';
+        mode_buff[m_i++] = (m & S_IXGRP) ? 'x' : '-';
+        mode_buff[m_i++] = (m & S_IROTH) ? 'r' : '-';
+        mode_buff[m_i++] = (m & S_IWOTH) ? 'w' : '-';
+        mode_buff[m_i++] = (m & S_IXOTH) ? 'x' : '-';
+        mode_buff[m_i++] = 0;
+    }
+
     printf("  File: %s\n", name_buff);
-    printf("  size: %-16ldBlocks: %-16ldIO Block: %ld\n", s.st_blocks, s.st_blocks, s.st_blksize);
-    printf("Device: %-16ldInode: %-16ldLinks: %ld\n", s.st_rdev, s.st_ino, s.st_nlink);
-    printf("   Uid: %-16dGid: %d\n", s.st_uid, s.st_gid);
+    printf("  size: %-16ldBlocks: %-12ldIO Block: %ld\n", s.st_blocks, s.st_blocks, s.st_blksize);
+    printf("Device: %-16ldInode: %-13ldLinks: %ld\n", s.st_rdev, s.st_ino, s.st_nlink);
+    printf("Access: %-16sUid: %-15dGid: %d\n", mode_buff, s.st_uid, s.st_gid);
     printf("Access: %s", ctime(&s.st_atime));
     printf("Modify: %s", ctime(&s.st_mtime));
     printf("Change: %s", ctime(&s.st_ctime));
